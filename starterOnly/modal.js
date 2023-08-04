b6573013b12e006fb1467a8dd2fae03ac4be569f
Axel Croce <axel@getinshoot.com>
2023-08-04 12:19:44 +0200

    ADD: close modal when clicking on X

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,7 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+//Close modal on click
+closeBtn.addEventListener("click", function() {
+  modalbg.style.display = "none";
+});
